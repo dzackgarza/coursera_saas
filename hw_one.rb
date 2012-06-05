0359fe256e01490738ce1393e178f92c3b21932a
Zack Garza <dzackgarza@gmail.com>
2012-06-04 20:27:25 -0700

    Cartesian products

--- hw_one.rb
+++ hw_one.rb
@@ -198,17 +198,31 @@ end
 #puts [1,2,3,2,1].palindrome?
 
 class CartesianProduct
-include Enumerable
-# your code here
+  include Enumerable
+    def initialize(set_one, set_two)
+      @set_one = set_one
+      @set_two = set_two
+    end
+    
+    def each
+     @set_one.each do |i|
+        @set_two.each do |j|
+          yield [i,j]
+        end
+      end
+    end
 end
-#Examples of use
-c = CartesianProduct.new([:a,:b], [4,5])
-c.each { |elt| puts elt.inspect }
+
+#c = CartesianProduct.new([:a,:b], [4,5])
+#c.each { |elt| puts elt.inspect }
+#  =>
 # [:a, 4]
 # [:a, 5]
 # [:b, 4]
 # [:b, 5]
-c = CartesianProduct.new([:a,:b], [])
-c.each { |elt| puts elt.inspect }
+
+#c = CartesianProduct.new([:a,:b], [])
+#c.each { |elt| puts elt.inspect }
+# =>
 # (nothing printed since Cartesian product
 # of anything with an empty collection is empty)
