6d3d73262c7ccb181f74febb48acafcd0d5663af
Zack Garza <dzackgarza@gmail.com>
2012-06-04 19:47:05 -0700

    attr_history method, extension of Numeric class for currency conversion

--- hw_one.rb
+++ hw_one.rb
@@ -90,5 +90,95 @@ def combine_anagrams(words)
   return output_list
 end
 
-anagram = ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']
-puts combine_anagrams(anagram)
+#anagram = ['cars', 'for', 'potatoes', 'racs', 'four','scar', 'creams', 'scream']
+#puts combine_anagrams(anagram)
+
+class Desserts
+  def initialize(name, calories)
+    @name = name
+    @calories = calories
+  end
+  attr_accessor :name, :calories
+  
+  def healthy?
+    if self.calories < 200 
+      return true
+    else
+      return false
+    end
+  end
+  
+  def delicious?
+    return true
+  end
+end
+
+class Jellybean < Desserts
+  def initialize(name, calories, flavor)
+    @name = name
+    @calories = calories
+    @flavor = flavor  
+  end
+  attr_accessor :flavor
+  def delicious?
+    if self.flavor == "black licorice"
+      return false
+    else 
+      return true
+    end
+  end
+end
+
+class Class
+  def attr_accessor_with_history(attr_name)
+    attr_name = attr_name.to_s # make sure it's a string
+    attr_reader attr_name # create the attribute's getter
+    attr_reader attr_name+"_history" # create bar_history getter
+    class_eval %Q{
+      def #{attr_name}=(attr_name)
+        @attr_name = attr_name
+        
+        if @#{attr_name}_history != nil
+          @#{attr_name}_history.push(attr_name)
+        else
+          @#{attr_name}_history = Array.new
+          @#{attr_name}_history.push(nil)
+          @#{attr_name}_history.push(attr_name)
+        end  
+      end
+    }
+  end
+end
+
+class Foo
+  attr_accessor_with_history :bar
+end
+
+#f = Foo.new
+#f.bar = 1
+#f.bar = 2
+#f.bar_history # => if your code works, should be [nil,1,2]
+
+class Numeric
+  @@currencies = {'dollar' => 1.000, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+ 
+  def in(currency_to_change_to)
+    singular_currency = currency_to_change_to.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(singular_currency)
+      self / @@currencies[singular_currency]
+    else
+      super
+    end
+  end
+  
+  def method_missing(method_id)
+    base_singular_currency = method_id.to_s.gsub( /s$/, '')
+    if @@currencies.has_key?(base_singular_currency)
+      self * @@currencies[base_singular_currency]
+    else
+      super
+    end
+  end
+end
+
+puts 1.292.dollars.in(:euros)
